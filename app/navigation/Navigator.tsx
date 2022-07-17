fd7d7d67db49365f9f7cfef9d54883b75049e9c9
RAMraghuram <ram309591@gmail.com>
2022-07-18 01:45:00 +0530

    Bottom tab bar icons and help page UI

--- app/navigation/Navigator.tsx
+++ app/navigation/Navigator.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import {DefaultTheme, NavigationContainer} from '@react-navigation/native';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import Icon from 'react-native-vector-icons/FontAwesome';
 import {
   ForgotScreen,
   LoginScreen,
@@ -45,10 +46,50 @@ const TabNavigator = () => {
         tabBarIconStyle: {marginTop: 6},
         headerShown: false,
       }}>
-      <Tab.Screen name="Home" component={HomeScreen} />
-      <Tab.Screen name="Map Plotting" component={MapPlottingScreen} />
-      <Tab.Screen name="Profile" component={ProfileScreen} />
-      <Tab.Screen name="Help" component={HelpScreen} />
+      <Tab.Screen
+        name="Home"
+        component={HomeScreen}
+        options={{
+          headerTitle: 'Home',
+          tabBarLabel: '',
+          tabBarIcon: ({color, size}) => (
+            <Icon name="home" color={color} size={size} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Map Plotting"
+        component={MapPlottingScreen}
+        options={{
+          headerTitle: 'map-marker',
+          tabBarLabel: '',
+          tabBarIcon: ({color, size}) => (
+            <Icon name="map-marker" color={color} size={size} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Profile"
+        component={ProfileScreen}
+        options={{
+          headerTitle: 'My Profile',
+          tabBarLabel: '',
+          tabBarIcon: ({color, size}) => (
+            <Icon name="user" color={color} size={size} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Help"
+        component={HelpScreen}
+        options={{
+          headerTitle: 'My Profile',
+          tabBarLabel: '',
+          tabBarIcon: ({color, size}) => (
+            <Icon name="user" color={color} size={size} />
+          ),
+        }}
+      />
     </Tab.Navigator>
   );
 };
@@ -79,7 +120,7 @@ const AuthStackNavigator = () => {
 const Navigator: React.FC = () => {
   return (
     <NavigationContainer theme={MyTheme}>
-      <AuthStackNavigator />
+      <TabNavigator />
     </NavigationContainer>
   );
 };
