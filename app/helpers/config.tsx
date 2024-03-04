40784af1021b095cdce366b46a89745b1248a44b
Your Name <your@email.com>
2024-03-04 11:14:37 -0600

    "Android updates - React Native Vector Icons updated changes"

--- app/helpers/config.tsx
+++ app/helpers/config.tsx
@@ -9,19 +9,16 @@ const commonConfig = {};
 
 const prodConfig = {
   ...commonConfig,
-  API_BASE_URL: 'https://icrops.globalbiomeservices.com',
+  API_BASE_URL: 'http://alb-icrops-1103729080.us-east-1.elb.amazonaws.com/',
 };
 
 const developmentConfig = {
   ...commonConfig,
-  API_BASE_URL: 'https://icrops.globalbiomeservices.com',
+  // API_BASE_URL: 'https://icrops.globalbiomeservices.com',
+  API_BASE_URL: 'http://alb-icrops-1103729080.us-east-1.elb.amazonaws.com/',
 };
 
 const AppConfig =
-  AppEnvironment === AppEnv.DEV
-    ? developmentConfig
-    : AppEnvironment === AppEnv.PROD
-      ? prodConfig
-      : prodConfig;
+  AppEnvironment !== AppEnv.PROD ? developmentConfig : prodConfig;
 
 export {AppConfig, AppEnvironment, AppEnv};
